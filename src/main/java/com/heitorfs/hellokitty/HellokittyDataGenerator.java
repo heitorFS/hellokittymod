6e3236263aa385eba8ddfa3bc909bd909792e076
heitorFS <heitor.freitassilva@outlook.com>
2025-03-26 16:25:34 -0300

    Initial commit

---
+++ src/main/java/com/heitorfs/hellokitty/HellokittyDataGenerator.java
@@ -0,0 +1,11 @@
+package com.heitorfs.hellokitty;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HellokittyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
