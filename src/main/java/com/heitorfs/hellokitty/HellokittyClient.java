8785b4ff8087c2b2d3c15fb87b881b442bd5d477
heitorFS <heitor.freitassilva@outlook.com>
2025-04-02 15:35:43 -0300

    client added

---
+++ src/main/java/com/heitorfs/hellokitty/HellokittyClient.java
@@ -0,0 +1,10 @@
+package com.heitorfs.hellokitty;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HellokittyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
