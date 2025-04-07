e956727223580c645c92bcf317a97fd9bb5e6c44
heitorFS <heitor.freitassilva@outlook.com>
2025-04-07 17:10:30 -0300

    Block and items tags update

--- src/main/java/com/heitorfs/hellokitty/datagen/ModBlockTagProvider.java
+++ src/main/java/com/heitorfs/hellokitty/datagen/ModBlockTagProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -40,5 +41,9 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.PLANKS)
                 .add(ModBlocks.BLUE_OAK_PLANKS)
                 .add(ModBlocks.RED_MAPLE_PLANKS);
+
+        getOrCreateTagBuilder(BlockTags.LEAVES)
+                .add(ModBlocks.BLUE_OAK_LEAVES)
+                .add(ModBlocks.RED_MAPLE_LEAVES);
     }
 }
--- src/main/java/com/heitorfs/hellokitty/datagen/ModItemTagProvider.java
+++ src/main/java/com/heitorfs/hellokitty/datagen/ModItemTagProvider.java
@@ -1,8 +1,11 @@
 package com.heitorfs.hellokitty.datagen;
 
+import com.heitorfs.hellokitty.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +16,16 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ItemTags.PLANKS)
+                .add(ModBlocks.BLUE_OAK_PLANKS.asItem())
+                .add(ModBlocks.RED_MAPLE_PLANKS.asItem());
 
+        getOrCreateTagBuilder(ItemTags.LOGS)
+                .add(ModBlocks.BLUE_OAK_LOG.asItem())
+                .add(ModBlocks.RED_MAPLE_LOG.asItem());
+
+        getOrCreateTagBuilder(ItemTags.LEAVES)
+                .add(ModBlocks.BLUE_OAK_LEAVES.asItem())
+                .add(ModBlocks.RED_MAPLE_LEAVES.asItem());
     }
 }
