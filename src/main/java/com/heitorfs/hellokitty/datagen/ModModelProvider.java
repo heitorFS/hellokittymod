de16d583cc4661779ba50d3bb77f1059e65567ef
heitorFS <heitor.freitassilva@outlook.com>
2025-04-07 14:22:48 -0300

    Model provider datagen

--- src/main/java/com/heitorfs/hellokitty/datagen/ModModelProvider.java
+++ src/main/java/com/heitorfs/hellokitty/datagen/ModModelProvider.java
@@ -1,9 +1,11 @@
 package com.heitorfs.hellokitty.datagen;
 
+import com.heitorfs.hellokitty.block.ModBlocks;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.TexturedModel;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -12,7 +14,17 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSingleton(ModBlocks.BLUE_OAK_LOG, TexturedModel.END_FOR_TOP_CUBE_COLUMN);
+        blockStateModelGenerator.registerSingleton(ModBlocks.STRIPPED_BLUE_OAK_LOG, TexturedModel.END_FOR_TOP_CUBE_COLUMN);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.BLUE_OAK_PLANKS);
+        blockStateModelGenerator.registerSingleton(ModBlocks.BLUE_OAK_LEAVES, TexturedModel.LEAVES);
+        blockStateModelGenerator.registerTintableCross(ModBlocks.BLUE_OAK_SAPLING, BlockStateModelGenerator.CrossType.NOT_TINTED);
 
+        blockStateModelGenerator.registerSingleton(ModBlocks.RED_MAPLE_LOG, TexturedModel.END_FOR_TOP_CUBE_COLUMN);
+        blockStateModelGenerator.registerSingleton(ModBlocks.STRIPPED_RED_MAPLE_LOG, TexturedModel.END_FOR_TOP_CUBE_COLUMN);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RED_MAPLE_PLANKS);
+        blockStateModelGenerator.registerSingleton(ModBlocks.RED_MAPLE_LEAVES, TexturedModel.LEAVES);
+        blockStateModelGenerator.registerTintableCross(ModBlocks.BLUE_OAK_SAPLING, BlockStateModelGenerator.CrossType.NOT_TINTED);
     }
 
     @Override
