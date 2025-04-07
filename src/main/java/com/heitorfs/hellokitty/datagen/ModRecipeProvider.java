c7dfb594c2bdd3aa6b1a9c51758990c2038a1ffc
heitorFS <heitor.freitassilva@outlook.com>
2025-04-07 17:09:16 -0300

    logs to planks crafting recipe

--- src/main/java/com/heitorfs/hellokitty/datagen/ModRecipeProvider.java
+++ src/main/java/com/heitorfs/hellokitty/datagen/ModRecipeProvider.java
@@ -1,9 +1,13 @@
 package com.heitorfs.hellokitty.datagen;
 
+import com.heitorfs.hellokitty.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.data.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -15,11 +19,24 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup wrapperLookup, RecipeExporter recipeExporter) {
-        return null;
+        return new RecipeGenerator(wrapperLookup, recipeExporter) {
+            @Override
+            public void generate() {
+                createShapeless(RecipeCategory.BUILDING_BLOCKS, ModBlocks.BLUE_OAK_PLANKS)
+                        .input(ModBlocks.BLUE_OAK_LOG)
+                        .criterion(hasItem(ModBlocks.BLUE_OAK_LOG), conditionsFromItem(ModBlocks.BLUE_OAK_LOG))
+                        .offerTo(recipeExporter);
+
+                createShapeless(RecipeCategory.BUILDING_BLOCKS, ModBlocks.RED_MAPLE_PLANKS)
+                        .input(ModBlocks.RED_MAPLE_LOG)
+                        .criterion(hasItem(ModBlocks.RED_MAPLE_LOG), conditionsFromItem(ModBlocks.RED_MAPLE_LOG))
+                        .offerTo(recipeExporter);
+            }
+        };
     }
 
     @Override
     public String getName() {
-        return "";
+        return "Recipes";
     }
 }
