5eaf12e3bdb985de8c88e537d6a3265cd9b9c858
heitorFS <heitor.freitassilva@outlook.com>
2025-04-07 17:10:09 -0300

    useless import removal

--- src/main/java/com/heitorfs/hellokitty/block/ModBlocks.java
+++ src/main/java/com/heitorfs/hellokitty/block/ModBlocks.java
@@ -11,7 +11,6 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
